382813003717701c84166f3a60b749661cb9359f
ckeller22 <chriskeller222@gmail.com>
2021-07-29 18:05:20 -0700

    inital implementation of event bus.

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { randomBytes } = require("crypto");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -16,14 +17,30 @@ app.get("/posts/:id/comments", (req, res) => {
 app.post("/posts/:id/comments", (req, res) => {
 	const commentId = randomBytes(4).toString("hex");
 	const { content } = req.body;
+	const postId = req.params.id;
 
-	const comments = commentsByPostId[req.params.id] || [];
+	const comments = commentsByPostId[postId] || [];
 
 	comments.push({ id: commentId, content });
 
-	commentsByPostId[req.params.id] = comments;
+	commentsByPostId[postId] = comments;
+
+	axios.post("http://localhost:4005/events", {
+		type: "CommentCreated",
+		data: {
+			id: commentId,
+			content,
+			postId,
+		},
+	});
 
 	res.status(201).send(comments);
+});
+
+app.post("/events", (req, res) => {
+	console.log("Received event", req.body.type);
+
+	res.send({});
 });
 
 app.listen(4001, () => {
